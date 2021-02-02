0123c87f803667790df07700de6326a11219560b
medha ajoy <medha.ajoy@navexglobal.com>
2021-02-02 13:58:15 +0530

    Commit all

---
+++ Netcore/Netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
